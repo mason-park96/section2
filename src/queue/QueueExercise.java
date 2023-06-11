1bbd3d8cda742d9460fe3bd9d2c303997c2f07b2
mason <antmddl12@gmail.com>
2023-06-11 10:26:42 +0900

    queue 시작

---
+++ src/queue/QueueExercise.java
@@ -0,0 +1,12 @@
+package queue;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Queue;
+
+public class QueueExercise {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new Queue<>();
+
+    }
+}
